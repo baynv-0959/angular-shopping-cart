c70a585f76cc6d8e9bd118524ba343259f30dd62
baynv-0959 <nguyen.van.bay@sun-asterisk.com>
2020-04-16 10:46:27 +0700

    find by size

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -9,6 +9,8 @@ import { Observable, of } from 'rxjs';
 })
 export class ProductService {
   private productsUrl = 'api/products';
+  private sizeUrl = 'api/sizes';
+  products: Product[];
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,7 +18,22 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts (): Observable<Product[]> {
+  getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl);
   }
+
+  getSizes(): Observable<String[]> {
+    return this.http.get<String[]>(this.sizeUrl);
+  }
+
+  filterProduct(...options): Observable<Product[]> {
+    let urlFilter = "api/products?availableSizes=(" + options[0]["sizes"].map((size) => size["name"]).join("|") + ")";
+
+    return this.http.get<Product[]>(urlFilter);
+  }
+}
+
+function difference(a1, a2) {
+  var a2Set = new Set(a2);
+  return a1.filter(function (x) { return !a2Set.has(x); });
 }
