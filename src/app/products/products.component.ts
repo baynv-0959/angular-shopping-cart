2a747847b28e41dddeabfac769769f93eab2d1e7
baynv-0959 <nguyen.van.bay@sun-asterisk.com>
2020-04-17 00:03:34 +0700

    add to cart remove cart and total price

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { Product } from '../shared/models/product'
 import { Size } from '../shared/models/size'
+import { Cart } from "../shared/models/cart";
 
 @Component({
   selector: 'app-products',
@@ -9,10 +10,13 @@ import { Size } from '../shared/models/size'
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  productLoadding = true;
+  inProgressSort = true;
+  inProgressLoadCart = false;
+  showCart = false;
   sizes: Size[];
   products: Product[];
-  productLoadding = true
-  inProgressSort = true
+  carts: Cart[];
   items: any[] = [
     { id: 0, name: 'Select' },
     { id: 1, name: 'Lowest to highest' },
@@ -25,6 +29,7 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.getSizes();
     this.getProducts();
+    this.getCarts();
   }
 
   getSizes(): void {
@@ -62,6 +67,42 @@ export class ProductsComponent implements OnInit {
       });
   }
 
+  getCarts(): void {
+    this.inProgressLoadCart = true;
+    let cartJson = localStorage.getItem('carts');
+    this.carts = JSON.parse(cartJson) || [];
+    this.inProgressLoadCart = false;
+  }
+
+  addToCart(product: Product): void {
+    this.inProgressLoadCart = true;
+    let product_ids = this.carts.map((cart) => {
+      return cart.product.id;
+    })
+
+    if(product_ids.includes(product.id)) {
+      const isLargeNumber = (element) => element.product.id == product.id;
+      let index = this.carts.findIndex(isLargeNumber);
+      this.carts[index]["count"] ++;
+    } else {
+      this.carts.push({product: product, count: 1});
+    }
+
+    this.inProgressLoadCart = false;
+    this.showCart = true;
+    localStorage.setItem('carts', JSON.stringify(this.carts));
+  }
+
+  removeCart(cart: Cart): void {
+    this.inProgressLoadCart = true;
+    const isLargeNumber = (element) => element == cart;
+    let index = this.carts.findIndex(isLargeNumber);
+    if (index !== -1) this.carts.splice(index, 1);
+    this.inProgressLoadCart = false;
+    debugger
+    localStorage.setItem('carts', JSON.stringify(this.carts));
+  }
+
   onSelect(index: number, isChecked: boolean): void {
     this.productLoadding = true;
     this.sizes[index]["isSelected"] = isChecked;
@@ -74,5 +115,21 @@ export class ProductsComponent implements OnInit {
 
   get loadSuccess(): boolean {
     return !(this.productLoadding && this.inProgressSort);
+  }
+
+  totalPrice(): number {
+    if (this.carts.length == 0) return 0;
+
+    return this.carts.map((cart) => {
+      return cart.product.price * cart.count
+    }).reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
+
+  countProducts(): number {
+    if (this.carts.length == 0) return 0;
+
+    return this.carts.map((cart) => {
+      return cart.count
+    }).reduce((accumulator, currentValue) => accumulator + currentValue);
   }
 }
